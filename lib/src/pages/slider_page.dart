3b4b6b17d670dce91837902c0039946e555f4926
jonytrifulca <jonytrifulca@hotmail.com>
2022-11-18 12:31:40 +0100

    add checkbox and switch examples

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -9,6 +9,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _sliderValue = 10;
+  bool? _checkEnabled = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +20,12 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(children: <Widget>[
           _createSlider(),
           Text("current value: $_sliderValue"),
-          Divider(height: 30.0,),
-          Expanded(child:_createImage())
+          Divider(
+            height: 30.0,
+          ),
+          _createCheckbox(),
+          _createSwitch(),
+          Expanded(child: _createImage())
         ]),
       ),
     );
@@ -33,19 +38,51 @@ class _SliderPageState extends State<SliderPage> {
         value: _sliderValue,
         min: 10.0,
         max: 800.0,
-        onChanged: (double currentSliderValue) {
-          print(currentSliderValue);
-          setState(() {
-            _sliderValue = currentSliderValue;
-          });
-        });
+        onChanged: (_checkEnabled == false)
+            ? null
+            : (double currentSliderValue) {
+                print(currentSliderValue);
+                setState(() {
+                  _sliderValue = currentSliderValue;
+                });
+              });
   }
 
   _createImage() {
     return Image(
-      image: NetworkImage("http://cdn.shopify.com/s/files/1/0535/2738/0144/articles/shutterstock_1290320698.jpg?v=1651099282"),
-      width: _sliderValue,
-      fit: BoxFit.contain
-    );
+        image: NetworkImage(
+            "http://cdn.shopify.com/s/files/1/0535/2738/0144/articles/shutterstock_1290320698.jpg?v=1651099282"),
+        width: _sliderValue,
+        fit: BoxFit.contain);
   }
+
+  Widget _createCheckbox() {
+    // return Checkbox(
+    //     value: _checkEnabled,
+    //     onChanged: (bool? currentCheckValue) {
+    //       setState(() {
+    //         _checkEnabled = currentCheckValue;
+    //       });
+    //     });
+
+    return CheckboxListTile(
+        title: Text("Block Slider"),
+        value: _checkEnabled,
+        onChanged: (bool? currentCheckValue) {
+          setState(() {
+            _checkEnabled = currentCheckValue;
+          });
+        });
+  }
+  
+ Widget _createSwitch() {
+      return SwitchListTile(
+        title: Text("Block Slider Switch version"),
+        value: (_checkEnabled != null && _checkEnabled == true) ? true: false,
+        onChanged: (bool? currentCheckValue) {
+          setState(() {
+            _checkEnabled = currentCheckValue;
+          });
+        });
+ }
 }
