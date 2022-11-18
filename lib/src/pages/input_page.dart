91cae630556b19e18fa1beb668bf7ffa60977b2e
jonytrifulca <jonytrifulca@hotmail.com>
2022-11-18 10:33:36 +0100

    input about email and password added

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -9,6 +9,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _name = "";
+  String _email = "";
+  String _password = "";
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +20,10 @@ class _InputPageState extends State<InputPage> {
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
           children: [
             _createInput(),
+            Divider(),
+            _createEmail(),
+            Divider(),
+            _createPassword(),
             Divider(),
             _createPerson()
           ],
@@ -38,17 +44,57 @@ class _InputPageState extends State<InputPage> {
           icon: Icon(Icons.account_circle)),
       onChanged: (value) {
         setState(() {
-           _name = value;
-           print(_name);
+          _name = value;
+          print(_name);
         });
-
       },
     );
   }
-  
+
   Widget _createPerson() {
     return ListTile(
       title: Text("The name is: $_name"),
+      subtitle: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("The email is: $_email"),
+            Text("The password is: $_password"),
+          ],
+        ),
     );
+  }
+
+  Widget _createEmail() {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            hintText: "Introduce email",
+            helperText: "instructions about the email",
+            label: Text("Email"),
+            suffixIcon: Icon(Icons.alternate_email),
+            icon: Icon(Icons.email)),
+        onChanged: (value) => setState(() {
+              _email = value;
+              print(_email);
+            }));
+  }
+
+  Widget _createPassword() {
+    return TextField(
+        obscureText: true,
+        keyboardType: TextInputType.visiblePassword,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+            hintText: "Introduce password",
+            helperText: "instructions about the password",
+            label: Text("Password"),
+            suffixIcon: Icon(Icons.lock_open),
+            icon: Icon(Icons.lock)),
+        onChanged: (value) => setState(() {
+              _password = value;
+              print(_email);
+            }));
   }
 }
