4d464f9f5610a9893e4710a140b4d61c78f54bea
jonytrifulca <jonytrifulca@hotmail.com>
2022-11-18 16:34:17 +0100

    added a scrollcontroller to the listView to manage when the user reachs the end of the list

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -8,7 +8,23 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
-  List<int> _numbersList = [1, 2, 3, 4, 5, 6, 7];
+  List<int> _numbersList = [];
+  int _lastNumber = 0;
+  ScrollController _scrollController = ScrollController();
+  // It is called in the onInit
+  @override
+  void initState() {
+    super.initState();
+    _add10MoreImages();
+    _scrollController.addListener(() {
+      //print("scroll executed");
+      //if we are in the end of the page....
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _add10MoreImages();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +36,30 @@ class _ListViewPageState extends State<ListViewPage> {
 
   Widget _createList() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numbersList.length,
       itemBuilder: (BuildContext context, int index) {
         final imageIndex = _numbersList[index];
-        return FadeInImage(
-            placeholder: AssetImage('assets/image_loading.gif'),
-            image: NetworkImage("https://picsum.photos/500/300?random=$imageIndex"));
+        return Column(
+          children: [
+            FadeInImage(
+                placeholder: AssetImage('assets/image_loading.gif'),
+                image: NetworkImage(
+                    "https://picsum.photos/500/300?random=$imageIndex")),
+            Text("- Image number: $imageIndex -"),
+            SizedBox(height: 10.0)        
+          ],
+        );
       },
     );
+  }
+
+  void _add10MoreImages() {
+    for (var i = 1; i < 10; i++) {
+      _lastNumber++;
+      _numbersList.add(_lastNumber);
+    }
+
+    setState(() {});
   }
 }
